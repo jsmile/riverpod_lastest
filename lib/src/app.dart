e600f6d50e68ea0afadb8e816a416b88422e27d0
Byeong Gui Jeong <jsmile@naver.com>
2023-11-14 20:28:26 +0900

    FutureProvider - Stateless ComsumerWidget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './screens/home_page.dart';
+import './screens/home_widgetref_page.dart';
+// import './screens/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -13,7 +14,8 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const HomeConsumerWidgetPage(),
+      // home: const HomePage(),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
